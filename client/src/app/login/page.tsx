1392d3ec8591a292efed2fee989884cedf311d7e
jainnaman-jain <namanjuly.23@gmail.com>
2025-07-29 13:17:35 +0530

    last commits

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     setIsLoading(true);
 
     try {
-      const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`;
+      const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}api/auth/login`;
       const response = await fetch(apiUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
